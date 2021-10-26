462c8dc1e2ce1418bba50959774f7c2fabafa191
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2021-10-27 01:11:44 +0700

    Creating EPC Application and Login Form

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/vendor/bootstrap5/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/vendor/fontawesome-free/css/all.min.css">
+    {{-- CSS --}}
+    <link rel="stylesheet" href="/css/style.css">
+
+    <link rel="shortcut icon" href="/img/epw-logo/logo-bg-blue.png" type="image/x-icon">
+
+    <title>EPW 2022</title>
+  </head>
+  <body>
+    {{-- Navbar --}}
+    <nav class="navbar navbar-expand-md py-0">
+      <div class="container">
+        <div class="navbar-brand flex-md-grow-1">
+          <a href="#">
+            <img src="/img/epw-logo/logo-white.png" alt="EPW Logo" width="60">
+          </a>
+        </div>
+        <div class="navbar-rainbow">
+          <div class="rainbow"></div>
+          <div class="rainbow"></div>
+          <div class="rainbow"></div>
+          <div class="rainbow"></div>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="fas fa-bars fa-lg fa-fw"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-md-grow-0" id="navbarNav">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item text-center">
+              <a class="nav-link my-3" href="/home">Back to Main Website</a>
+            </li>
+            <li class="nav-item text-center dropdown">
+              <a class="nav-link dropdown-toggle my-3" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Other Event
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item text-center" href="/epc">EPC</a></li>
+                <li><a class="dropdown-item text-center" href="/snow">SNOW</a></li>
+                <li><a class="dropdown-item text-center" href="/">Grand Roadshow</a></li>
+                <li><a class="dropdown-item text-center" href="/">Big Event</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    {{-- End of Navbar --}}
+
+    @yield('content')
+
+    {{-- Footer --}}
+    {{-- <footer class="footer footer-blue">
+      <div class="container pt-4 pb-2">
+        <div class="row align-items-center">
+          <div class="col-lg-4 text-center">
+            <img src="/img/epw-logo/logo-white.png" alt="EPW Logo" class="footer-logo">
+          </div>
+          <div class="col-lg-8">
+            <div class="footer-desc">
+              <h2 class="footer-title">Reach us!</h2>
+              <a href="https://goo.gl/maps/SBkiPs6v96jUeDHn9" target="_blank" class="footer-content">
+                <img src="/svg/icons/location.svg" alt="" class="footer-icon">
+                <p>
+                  Department of Engineering Physics<br>
+                  <span>Kampus ITS, Keputih, Kec. Sukolilo, Surabaya, Jawa Timur - 60111</span>
+                </p>
+              </a>
+              <div class="row">
+                <div class="col-md-4 col-6">
+                  <a href="https://www.instagram.com/epw.its/" target="_blank" class="footer-content">
+                    <img src="/svg/icons/instagram.svg" alt="" class="footer-icon">
+                    <span>@epw.its</span>
+                  </a>
+                </div>
+                <div class="col-md-4 col-6">
+                  <a href="http://line.me/ti/p/~@ket9005b" target="_blank" class="footer-content">
+                    <img src="/svg/icons/line.svg" alt="" class="footer-icon">
+                    <span>@ket9005b</span>
+                  </a>
+                </div>
+                <div class="col-md-4 col-6">
+                  <a href="https://www.linkedin.com/in/epw-its/" target="_blank" class="footer-content">
+                    <img src="/svg/icons/linkedin.svg" alt="" class="footer-icon">
+                    <span>EPW ITS</span>
+                  </a>
+                </div>
+                <div class="col-md-4 col-6">
+                  <a href="https://twitter.com/epw_its" target="_blank" class="footer-content">
+                    <img src="/svg/icons/twitter.svg" alt="" class="footer-icon">
+                    <span>@epw_its</span>
+                  </a>
+                </div>
+                <div class="col-md-4 col-6">
+                  <a href="https://www.tiktok.com/@epw.its" target="_blank" class="footer-content">
+                    <img src="/svg/icons/tiktok.svg" alt="" class="footer-icon">
+                    <span>@epw.its</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p class="text-center mt-5 copyright">Copyright &copy; 2021 by EPW ITS 2022, all rights reserved!</p>
+      </div>
+    </footer> --}}
+    {{-- End of Footer --}}
+    
+    {{-- Javascript --}}
+    <script src="/vendor/bootstrap5/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main')
+
+@section('content')
+    
+  <section id="loginForm">
+    <div class="container d-flex justify-content-center flex-column py-5">
+      <div class="row align-items-center">
+        <div class="col-md-6 px-5">
+          <img src="/img/logo-epc.svg" class="login-logo d-block mx-auto" alt="">
+          <img src="/img/epc-2022.png" class="login-epw-2022 d-block mx-auto" alt="">
+        </div>
+        <div class="col-md-6 px-4">
+          <form action="" method="POST" class="login-form mx-auto mt-5 mt-md-0">
+            <div class="mb-4">
+              <label for="username" class="form-label">Username</label>
+              <input class="form-control" type="text" id="username" name="username" placeholder="Username">
+            </div>
+            <div class="mb-4">
+              <label for="password" class="form-label">Password</label>
+              <input class="form-control" type="text" id="password" name="password" placeholder="Password">
+            </div>
+            <button type="submit" class="btn login-button w-100">Log In</button>
+            <p class="login-register text-center mt-3">Belum mendaftar? <a href="">Daftar sekarang!</a></p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+@endsection
